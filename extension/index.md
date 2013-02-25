e7f31fad4d4079e13ddef86d6669f2e56d54fe2a
yansong <yss.nelson@gmail.com>
2013-02-25 15:22:30 +0800

    add extension

---
+++ extension/index.md
@@ -0,0 +1,10 @@
+---
+layout: blog
+title: chrome插件说明
+tags: [chrome插件使用]
+summary: 
+---
+### 说明
+
+1. 打开在chrome下打开：<chrome://extensions/>
+2. 将下载好的<yss.crx>托到上面打开的页面即可。
---
+++ extension/yss/static/js/popup.js
@@ -0,0 +1,33 @@
+$(function() {
+    $(document.forms['refreshForm']).submit(function() {
+        exec(function refresh(time, url) {
+            time = time || 6;
+            url = url || window.location.href;
+            setInterval(run, time * 1000);
+            run();
+            function run() {
+                var body = document.body,
+                    height = body.clientHeight,
+                    width = body.clientWidth;
+                body.innerHTML = '<iframe src="' + url +'" height="' + height + '" width="' + width + '" frameborder="0" allowtransparent="true" scrolling="yes"></iframe>';
+            }
+        }, [this.time.value.trim(), this.url.value.trim()]);
+    });
+    // 放到页面执行脚本
+    function exec(fn, args) {
+        if (args) {
+            if (Object.prototype.toString.call(args) !== '[object Array]') {
+                console.log('Error arguments for exec function:', args);
+                args = '';
+            } else {
+                args = JSON.stringify(args);
+                args = args.substring(1, args.length-1);
+            }
+        }
+        // console.log(args);
+        // console.log(fn.toString() + ';' + fn.name + '('+ args +');')
+        chrome.tabs.executeScript(null, {
+            code: fn.toString() + ';' + fn.name + '('+ args +');'
+        });
+    }
+});
